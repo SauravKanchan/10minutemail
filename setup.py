0f068f3baeacc882745e8bdc78d706fd336c78e8
SauravKanchan <sauravnk30@gmail.com>
2021-05-10 17:08:17 +0530

    Add tox

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="python-10minutemail",
-    version="0.1.0",
+    version="0.1.1",
     description="Python wrapper for 10minutemail.com",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -25,11 +25,17 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
     ],
-    py_modules= ["minutemail", "endpoints"],
+    py_modules=["minutemail", "endpoints"],
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=[
         "requests >= 2.20"
     ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'tox'
+        ]
+    },
     entry_points={},
 )
